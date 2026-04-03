cfdac3d5ac0328c065730bf8a6a369c7e5c95b6b
Luis Henrique Gianeti <169113501+luishenriquegianeti@users.noreply.github.com>
2026-04-03 18:50:21 +0000

    03/04/2026 CSS

---
+++ RHDenuncia/config.php
@@ -0,0 +1,18 @@
+<?php
+$dbHost = 'localhost';
+$dbUsername = 'id21279264_rhdenuncia';
+$dbPassword = 'Mestre8812%';
+$dbName='id21279264_rhdenuncia';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+/*
+if($conexao->connect-errno)
+{
+    echo"erro";
+}
+else
+{
+    echo "correto";
+}
+*/
+?>
